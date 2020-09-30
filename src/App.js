c0a008c7625620cd6aa1b54190f812a247d453c4
Gianluca <gianlucazin1990@gmail.com>
2020-09-30 18:27:05 +0200

    grid created

--- src/App.js
+++ src/App.js
@@ -4,7 +4,54 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>This is my app</h1>
+      <div id="display">Display</div>
+      <div id="reset">Ac</div>
+      <div id="add" class="operators">
+        +
+      </div>
+      <div id="subtract" class="operators">
+        -
+      </div>
+      <div id="multiply" class="operators">
+        *
+      </div>
+      <div id="divide" class="operators">
+        /
+      </div>
+      <div id="equals">=</div>
+      <div id="decimal" class="number">
+        .
+      </div>
+      <div id="zero" class="number">
+        0
+      </div>
+      <div id="one" class="number">
+        1
+      </div>
+      <div id="two" class="number">
+        2
+      </div>
+      <div id="three" class="number">
+        3
+      </div>
+      <div id="four" class="number">
+        4
+      </div>
+      <div id="five" class="number">
+        5
+      </div>
+      <div id="six" class="number">
+        6
+      </div>
+      <div id="seven" class="number">
+        7
+      </div>
+      <div id="eigth" class="number">
+        8
+      </div>
+      <div id="nine" class="number">
+        9
+      </div>
     </div>
   );
 }
