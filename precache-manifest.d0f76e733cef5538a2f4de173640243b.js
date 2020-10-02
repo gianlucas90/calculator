1ebb9490f6d647de68552c386c366e70c2834e28
Gianluca <gianlucazin1990@gmail.com>
2020-10-02 09:42:45 +0200

    Updates

---
+++ precache-manifest.d0f76e733cef5538a2f4de173640243b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54899cc5d549a804ffd5deb808b38c79",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "c70c55df8a06002461ce",
+    "url": "/calculator/static/css/main.61fcc244.chunk.css"
+  },
+  {
+    "revision": "5584ba00af12491390bf",
+    "url": "/calculator/static/js/2.6847e2bc.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/calculator/static/js/2.6847e2bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c70c55df8a06002461ce",
+    "url": "/calculator/static/js/main.6c2d32ae.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
